46314407e7c655d3eea69a3ebf1ab31ec256a679
Christopher Jeffrey <chjjeffrey@gmail.com>
2012-07-10 13:52:36 -0500

    change default mock interface. add method names. handle unsupported features.

--- test/mock.js
+++ test/mock.js
@@ -1,45 +1,8 @@
 /**
- * Charged
- * High-level Chargify API Binding
- * Copyright (c) 2012, Christopher Jeffrey
+ * Mock
  */
 
-// http://docs.chargify.com/api-integration
-// http://docs.chargify.com/api-resources
-// http://docs.chargify.com/api-subscriptions
-// http://docs.chargify.com/subscriptions-intro
-// http://docs.chargify.com/api-subscriptions
-
-// EXAMPLE USAGE:
-// var charge = new Charged({
-//   product: 'monthly-chili-cheese-fries',
-//   key: 'ad3rt4302ebdd'
-// });
-// charge.readSubscription('chjj', function(err, result) {
-//   if (err) throw err;
-//   console.log(result);
-// });
-// var options = { memo: 'bill', amount_in_cents: 100 };
-// charge.charge('chjj', options, function(err, result) {
-//   if (err) throw err;
-//   console.log(result);
-// });
-
-var request = require('request');
-
-function Charged(options) {
-  if (!(this instanceof Charged)) {
-    return new Charged(options);
-  }
-
-  this.subdomain = options.subdomain || options.product || options.name;
-  this.apiKey = options.key || options.apiKey;
-
-  if (!this.subdomain || !this.apiKey) {
-    throw new
-      Error('Options `subdomain` and `apiKey` are required for Charged.');
-  }
-}
+var Charged = require('../');
 
 /**
  * Mock Data
@@ -80,164 +43,13 @@ var _component = {
 };
 
 /**
- * Customers
- */
-
-Charged.prototype.getCustomers =
-Charged.prototype.listCustomers = function(callback) {
-  return callback(null, [_customer]);
-};
-
-Charged.prototype.getCustomerById = function(name, callback) {
-  return callback(null, _customer);
-};
-
-Charged.prototype.getCustomerByRef = function(ref, callback) {
-  return callback(null, _customer);
-};
-
-Charged.prototype.deleteCustomerById = function(name, callback) {
-  return callback(null, _customer);
-};
-
-Charged.prototype.deleteCustomerByRef = function(ref, callback) {
-  return callback(null, _customer);
-};
-
-Charged.prototype.createCustomer = function(options, callback) {
-  return (callback || options)(null, _customer);
-};
-
-Charged.prototype.updateCustomer = function(name, options, callback) {
-  return (callback || options)(null, _customer);
-};
-
-Charged.prototype.updateCustomerByRef = function(ref, options, callback) {
-  return (callback || options)(null, _customer);
-};
-
-Charged.prototype.getCustomerSubscriptions = function(name, callback) {
-  return callback(null, [_subscription]);
-};
-
-/**
- * Subscriptions
- */
-
-Charged.prototype.getSubscriptions =
-Charged.prototype.listSubscriptions = function(callback) {
-  // XXX unsure of the formatter
-  return callback(null, [_subscription]);
-};
-
-Charged.prototype.createSubscription = function(options, callback) {
-  return (callback || options)(null, _subscription);
-};
-
-Charged.prototype.getSubscription =
-Charged.prototype.readSubscription = function(name, callback) {
-  return callback(null, _subscription);
-};
-
-// XXX This is probably slow.
-Charged.prototype.getSubscriptionByRef = function(name, callback) {
-  return callback(null, _subscription);
-};
-
-Charged.prototype.updateSubscription = function(name, options, callback) {
-  return (callback || options)(null, _subscription);
-};
-
-Charged.prototype.cancelSubscription = function(name, options, callback) {
-  return (callback || options)(null, _subscription);
-};
-
-Charged.prototype.reactivateSubscription = function(name, options, callback) {
-  return (callback || options)(null, _subscription);
-};
-
-Charged.prototype.migrateSubscription = function(name, options, callback) {
-  return (callback || options)(null, _subscription);
-};
-
-Charged.prototype.adjustSubscription = function(name, options, callback) {
-  return (callback || options)(null, _subscription);
-};
-
-Charged.prototype.charge =
-Charged.prototype.createCharge =
-Charged.prototype.chargeSubscription = function(name, options, callback) {
-  return (callback || options)(null, _subscription);
-};
-
-Charged.prototype.getSubscriptionComponents =
-Charged.prototype.listSubscriptionComponents = function(name, callback) {
-  return callback(null, [_component]);
-};
-
-Charged.prototype.getSubscriptionComponent = function(sub, comp, callback) {
-  return callback(null, _component);
-};
-
-Charged.prototype.getSubscriptionUsage =
-Charged.prototype.listSubscriptionUsage = function(sub, comp, callback) {
-  return callback(null, {});
-};
-
-Charged.prototype.getSubscriptionTransactions = function(name, callback) {
-  return callback(null, [_transaction]);
-};
-
-Charged.prototype.getSubscriptionStatements = function(name, callback) {
-  return callback(null, [_statement]);
-};
-
-/**
- * Transactions
- */
-
-Charged.prototype.getTransactions = function(name, callback) {
-  return callback(null, [_transaction]);
-};
-
-/**
- * Products
- */
-
-Charged.prototype.getProductFamilyComponents =
-Charged.prototype.listProductFamilyComponents = function(name, callback) {
-  // XXX unsure of the formatter
-  return callback(null, [_component]);
-};
-
-Charged.prototype.getProducts =
-Charged.prototype.listProducts = function(callback) {
-  return callback(null, [_product]);
-};
-
-Charged.prototype.getProduct = function(name, callback) {
-  return callback(null, _product);
-};
-
-Charged.prototype.getProductByHandle = function(handle, callback) {
-  return callback(null, _product);
-};
-
-/**
  * Request
  */
 
-Charged.prototype.request =
-Charged.prototype.get =
-Charged.prototype.put =
-Charged.prototype.post =
-Charged.prototype.delete = function() {
-  var args = Array.prototype.slice.call(arguments);
-  if (typeof args[args.length-1] === 'string') {
-    var type = args.pop();
-  }
+Charged.prototype.request = function(options, callback) {
   var result;
-  switch (type) {
+
+  switch (options.format) {
     case 'customer':
       result = _customer;
       break;
@@ -254,6 +66,14 @@ Charged.prototype.delete = function() {
       result = {};
       break;
   }
+
+  switch (options.path) {
+    case '/products':
+    // etc
+      result = [result];
+      break;
+  }
+
   return callback(null, result);
 };
 
